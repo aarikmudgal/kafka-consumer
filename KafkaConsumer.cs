88774760c5322b7997f9041a9619a423eee557ee
Mudgal Aarik <aarik.mudgal@metrosystems-external.net>
2018-04-05 16:09:36 +0530

    New

---
+++ KafkaConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace kafkaConsumer
+{
+    class KafkaConsumer
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var simpleConsumer = new SimpleConsumer();
+            simpleConsumer.Listen(Console.WriteLine);
+        }
+    }
+}
---
+++ SimpleConsumer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace kafkaConsumer
+{
+    class SimpleConsumer : ISimpleConsumer
+    {
+        public void Listen(Action<string> messgae)
+        {
+            var config = new Dictionary<string, object>
+            {
+                {"group.id", "simple_consumer"},
+                {"bootstrap.servers", "35.200.241.185:9092" },
+                {"enable.auto.commit", "false" }
+            };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.Subscribe("simpletest");
+                 
+                    
+                consumer.OnMessage += (_, msg) =>
+                {
+                    messgae(msg.Value);
+                };
+
+                while (true)
+                {
+                    consumer.Poll(100);
+                }
+            }
+        }
+
+       
+    }
+}
